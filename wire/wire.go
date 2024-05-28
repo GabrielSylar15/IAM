1d9dcf15a9cfb99407bdf8490f47db10bbc13038
vinhnt211 <vinhnt211@ghtk.co>
2024-05-29 01:46:46 +0700

    fix inject

--- wire/wire.go
+++ wire/wire.go
@@ -4,21 +4,13 @@
 //go:generate go run -mod=mod github.com/google/wire/cmd/wire
 package wire
 
-import (
-	"IAM/repository"
-	"IAM/routers/api"
-	"IAM/service"
-	"github.com/google/wire"
-	"github.com/jinzhu/gorm"
-)
-
 // using go run -mod=mod github.com/google/wire/cmd/wire to generate
 
-func InitializeApplicationController(db *gorm.DB) api.ApplicationController {
-	wire.Build(
-		api.NewApplicationController,
-		service.NewApplicationService,
-		repository.NewApplicationRepository,
-	)
-	return nil
-}
+//func InitializeApplicationController(db *gorm.DB) api.ApplicationController {
+//	wire.Build(
+//		api.NewApplicationController,
+//		service.NewApplicationService,
+//		repository.NewApplicationRepository,
+//	)
+//	return nil
+//}
