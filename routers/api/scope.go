f3df8a2e3b7941c3a691fdea8ac06851ff7fff22
vinhnt211 <vinhnt211@ghtk.co>
2024-06-11 01:09:00 +0700

    add scope

--- routers/api/scope.go
+++ routers/api/scope.go
@@ -1,22 +1,34 @@
 package api
 
 import (
+	"IAM/entities"
 	"IAM/service"
+	"IAM/utils"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type scopeController struct {
-	scopeService *service.ScopeService
+	scopeService service.ScopeService
 }
 
 type ScopeController interface {
 	CreateScope(c *gin.Context)
 }
 
-func InitScopeController(scopeService *service.ScopeService) ScopeController {
+func InitScopeController(scopeService service.ScopeService) ScopeController {
 	return &scopeController{scopeService}
 }
 
 func (c *scopeController) CreateScope(ctx *gin.Context) {
-	
+	var request entities.Scope
+	if err := ctx.BindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, utils.BuildInternalErrorResponse())
+		return
+	}
+	entity, err := c.scopeService.CreateScope(&request)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, utils.BuildInternalErrorResponse())
+	}
+	ctx.JSON(http.StatusOK, utils.BuildSuccessResponse(entity))
 }
--- service/scope_service.go
+++ service/scope_service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"IAM/entities"
+	"IAM/repository"
+)
+
+type scopeService struct {
+	scopeRepository repository.ScopeRepository
+}
+
+type ScopeService interface {
+	CreateScope(scope *entities.Scope) (entities.Scope, error)
+}
+
+func InitScopeService(scopeRepository repository.ScopeRepository) ScopeService {
+	return &scopeService{scopeRepository}
+}
+
+func (c *scopeService) CreateScope(scope *entities.Scope) (entities.Scope, error) {
+	return c.scopeRepository.CreateScope(scope)
+}
